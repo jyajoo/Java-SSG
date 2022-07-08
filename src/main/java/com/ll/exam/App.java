de64266a2e271e00d2d2414544531255a530afb4
jyajoo <jyajoo1020@gmail.com>
2022-07-08 14:19:39 +0900

    add: 테스트_스캐너

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -4,5 +4,34 @@ import java.util.Scanner;
 
 public class App {
     public void run() {
+        System.out.println("== 명언 SSG==");
+
+        Scanner sc = new Scanner(System.in);
+        int wiseSayingLastId = 0;
+
+        outer:
+        while (true) {
+            System.out.print("명령) ");
+            String cmd = sc.nextLine().trim();
+
+            switch (cmd) {
+                case "등록":
+                    System.out.print("명언 : ");
+                    String content = sc.nextLine().trim();
+                    System.out.print("작가 : ");
+                    String author = sc.nextLine().trim();
+                    int id = ++wiseSayingLastId;
+
+                    WiseSaying wiseSaying = new WiseSaying(id, content, author);
+
+                    System.out.println(id + "번 명령이 등록되었습니다.");
+                    break;
+
+                case "종료":
+                    break outer;
+            }
+        }
+
+        sc.close();
     }
 }
