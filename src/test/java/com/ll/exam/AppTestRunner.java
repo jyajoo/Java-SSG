9052d93d3d381e0330cc001c61bd6d075c213659
jyajoo <jyajoo1020@gmail.com>
2022-07-14 23:44:42 +0900

    docs: AppTestRunner & TestUtil 주석 추가

--- src/test/java/com/ll/exam/AppTestRunner.java
+++ src/test/java/com/ll/exam/AppTestRunner.java
@@ -5,14 +5,23 @@ import java.util.Scanner;
 
 public class AppTestRunner {
     public static String run(String input) {
+        /*
+        문자열을 입력으로 하는 Scanner 생성
+         */
         Scanner sc = TestUtil.genScanner(input);
 
+        /*
+        System.out이 특정 바이트스트림을 가리키도록 함
+         */
         ByteArrayOutputStream output = TestUtil.setOutToByteArray();
         new App(sc).run();
 
         String rs = output.toString();
-        TestUtil.clearSetOutToByteArray(output);
 
+        /*
+        System.out이 다시 제자리로 돌아가도록 함
+         */
+        TestUtil.clearSetOutToByteArray(output);
         return rs;
     }
 }
--- src/test/java/com/ll/exam/TestUtil.java
+++ src/test/java/com/ll/exam/TestUtil.java
@@ -5,12 +5,18 @@ import java.util.Scanner;
 
 public class TestUtil {
 
+    /*
+    문자열을 입력으로 하는 Scanner 생성
+     */
     public static Scanner genScanner(String input) {
         InputStream in = new ByteArrayInputStream(input.getBytes());
 
         return new Scanner(in);
     }
 
+    /*
+    System.out이 특정 바이트스트림을 가리키도록 함
+     */
     public static ByteArrayOutputStream setOutToByteArray() {
         final ByteArrayOutputStream output = new ByteArrayOutputStream();
         System.setOut(new PrintStream(output));
@@ -18,6 +24,9 @@ public class TestUtil {
         return output;
     }
 
+    /*
+    System.out이 다시 제자리로 돌아가도록 함
+     */
     public static void clearSetOutToByteArray(ByteArrayOutputStream output) {
         System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
         try {
