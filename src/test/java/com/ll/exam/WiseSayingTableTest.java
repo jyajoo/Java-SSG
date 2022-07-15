09bf96fb0626ee72cd38a0dae2349d8abce27d48
jyajoo <jyajoo1020@gmail.com>
2022-07-15 17:30:19 +0900

    add: BeforeEach로 초기화

--- src/test/java/com/ll/exam/WiseSayingTableTest.java
+++ src/test/java/com/ll/exam/WiseSayingTableTest.java
@@ -1,5 +1,6 @@
 package com.ll.exam;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -8,14 +9,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class WiseSayingTableTest {
 
-    @Test
-    public void 등록_출력() {
-        WiseSayingTable wiseSayingTable = new WiseSayingTable("test_data");
+    private WiseSayingTable wiseSayingTable;
 
+    public WiseSayingTableTest() {
+        wiseSayingTable = new WiseSayingTable("test_data");
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        Util.file.deleteDir("test_data");
         wiseSayingTable.save("삶이 있는 한 희망은 있다.", "키케로");
-        assertTrue(new File("test_data/wise_saying/1.json").exists());
-
         wiseSayingTable.save("나에게 불가능이란 없다.", "나폴레옹");
-        assertTrue(new File("test_data/wise_saying/2.json").exists());
+    }
+
+    @Test
+    public void 저장() {
+        int newId = wiseSayingTable.getLastId() + 1;
+        wiseSayingTable.save("자유가 아니면 죽음을 달라.", "패트릭 헨리");
+        assertTrue(new File("test_data/wise_saying/%d.json".formatted(newId)).exists());
     }
 }
